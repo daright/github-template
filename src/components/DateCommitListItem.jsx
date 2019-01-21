7e4d22d7fee2a8a4ca4070a5ef35b7d8f8f395a5
daright <piotr.kasperek@gmail.com>
2019-01-21 11:07:12 +0100

    adjustments

--- src/components/DateCommitListItem.jsx
+++ src/components/DateCommitListItem.jsx
@@ -4,16 +4,21 @@ import CommitListItem from './CommitListItem';
 
 export default function DateCommitListItem({ date, issues, toggleStar }) {
     return (
-        <div>
+        <StyledList>
             <StyledDate>{date}</StyledDate>
             {issues.map((issue, index) => (
                 <CommitListItem {...issue} key={index} index={index} toggleStar={toggleStar} />
             ))}
-        </div>
+        </StyledList>
     );
 }
 
-const StyledDate = styled.p`
+const StyledList = styled.ul`
+    margin: 0;
+    padding: 0;
+`;
+
+const StyledDate = styled.li`
     color: #8a8995;
     height: 30px;
     margin: 0;
